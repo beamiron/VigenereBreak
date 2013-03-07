421221547471784a6138304055b75687b5b64f56
Redmond <myinboxforchan@gmail.com>
2013-03-07 12:54:42 -0600

    Initial Commit
    Created and added the key length calculator.

---
+++ VinKeyLength.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#Calculate the length of the key for a vigenere ciphertext
+import math,sys
+def Shift(l,n): #Shifts List L by N positions
+	l=list(l)
+	Shifted=l[n:]+l[:n]
+	return Shifted
+def Frequency(p,k): #Finds the Frequency of K in set P
+		p = p.upper()
+		pLength = len(p)
+		CharacterCountArray = [0]*26
+		ActualFrequency = [0]*26
+		PossibleProbability = [0]* 26
+		for i in range (0,pLength):
+			CharacterCountArray[ord(p[i]) - 65]  = CharacterCountArray[ord(p[i]) - 65] + 1
+		for x in range (0,26):
+			ActualFrequency[x] = CharacterCountArray[x] / pLength
+		return ActualFrequency
+def CyclicShift(p,n): #Shifts set P by N movements and returns matches
+	p = p.upper()
+	OriginalText = list(p)
+	ShiftedText = Shift(OriginalText,n) 
+	pLength = len(p)
+	TotalMatches = 0
+	for i in range (0,pLength):
+		if OriginalText[i] == ShiftedText[i]:
+			TotalMatches += 1
+	return TotalMatches 
+
+#Actual Program starts here
+with open (sys.argv[1], "r") as myfile:
+    ChiperText=myfile.read().replace('\n', '')
+KeyLengthArray = [0] * 27
+for x in range (1,27):
+	CurrentKeyLength = CyclicShift(ChiperText,x)
+ 	KeyLengthArray[x] =  CurrentKeyLength
+HighestKeyCount = max(KeyLengthArray)
+KeyLength = KeyLengthArray.index(HighestKeyCount)
+print "Key length is : " + str(KeyLength)
